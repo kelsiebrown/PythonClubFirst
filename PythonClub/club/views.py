dfb8ab4295d48be53f2abae275078595492cc2a3
Kelsie Brown <38116231+kelsiebrown@users.noreply.github.com>
2019-01-30 10:31:53 -0800

    Add resource view

--- PythonClub/club/views.py
+++ PythonClub/club/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Resource
 
 # Create your views here.
 def index(request):
     return render(request, 'club/index.html')
+
+def resources (request):
+    type_list=Resource.objects.all()
+    return render (request, 'club/resources.html', {'type_list': type_list})
